4b59fd272cc2b2e41006c7ae22ea9a33c98c3a3e
unknown <imyers@MYERS2943.Student.Northface.local>
2013-07-12 18:46:13 -0600

    Reflection Activity

---
+++ first_distributed_system/imyers_reflection_activity/src/Main.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: imyers
+ * Date: 7/12/13
+ * Time: 5:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Class c = Class.forName("ReflectionActivity");
+
+        Constructor[] constructors = c.getDeclaredConstructors();
+
+        for(Constructor constructor : constructors) {
+            System.out.print(constructor.getName());
+            System.out.print("(");
+            for(int i = 0; i < constructor.getParameterTypes().length; i++) {
+                Class aClass = constructor.getParameterTypes()[i];
+                System.out.print(aClass.getName());
+                if(i < constructor.getParameterTypes().length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println(")");
+        }
+
+        Object instance = c.getConstructor(String.class, double.class).newInstance("sean", 4.5);
+
+        for(int i = 0; i < c.getDeclaredFields().length; i++) {
+            Field field = c.getDeclaredFields()[i];
+            System.out.println(field.getType().getName() + " " + field.getName());
+        }
+
+        Method meth = c.getDeclaredMethod("addToNumber", double.class);
+        System.out.println(meth.invoke(instance, 5));
+    }
+
+}
